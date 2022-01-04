823ff5eaec0d425fcbbdab1f58cfaacf0cb103a1
Daniel Serleg <daniel.serleg@bitrise.io>
2022-01-04 13:11:33 +0100

    Tcp socket send

--- plant-healthcheck-wifi/plant-healthcheck-wifi.ino
+++ plant-healthcheck-wifi/plant-healthcheck-wifi.ino
@@ -1,5 +1,6 @@
 #include "ESP8266WiFi.h"
 #include <ESP8266WebServer.h>
+#include "pages.h"
 
 ESP8266WebServer server(80);
 
@@ -7,11 +8,71 @@ void handleRoot();              // function prototypes for HTTP handlers
 void handleLogin();
 void handleNotFound();
 
+String readFromSerialIfAvailable();
+void processSerialCommand(String cmd);
+void configState();
+void operationState();
+bool sendDataToTCPServer(String message);
+
+String ssid;
+String password;
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
+  Serial.println("Module started");
 
+  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
+  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  WiFi.begin();
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+}
+
+void loop() {
+  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+  delay(3000);
+  server.handleClient();                     // Listen for HTTP requests from clients
+
+  String serialCmd = readFromSerialIfAvailable();
+
+  if (serialCmd.length() > 0) {
+    processSerialCommand(serialCmd);  
+  }
+}
+
+void processSerialCommand(String cmd) {
+  char cmdChar = cmd[0];
+  String message = "";
+
+  if (cmd.length() >2) {
+    message = cmd.substring(2, cmd.length());
+  }
+  
+  if (cmdChar == '0') {
+    configState();
+  }
+
+  if (cmdChar == '1') {
+    operationState();
+  }
+
+  if (cmdChar == '2') {
+    sendDataToTCPServer(message);
+  }
+}
+
+void configState() {
   Serial.print("Setting soft-AP ... ");
   boolean result = WiFi.softAP("ESPsoftAP_01", "123456789");
   if(!result)
@@ -26,19 +87,61 @@ void setup() {
   Serial.print("AP IP address: ");
   Serial.println(IP);
 
-
-  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/"
-  server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
-  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
-
   server.begin();                            // Actually start the server
   Serial.println("HTTP server started");
 }
 
-void loop() {
-  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-  delay(3000);
-  server.handleClient();                     // Listen for HTTP requests from clients
+void operationState() {
+  server.stop();
+  WiFi.softAPdisconnect (true);
+  
+  WiFi.setAutoReconnect(false);
+  WiFi.disconnect();
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+
+  WiFi.setAutoConnect(true);
+  WiFi.setAutoReconnect(true);
+}
+
+bool sendDataToTCPServer(String message) {
+  WiFiClient client;
+  const char * host = "192.168.88.207";
+  const int httpPort = 3333;
+
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return false;
+  }
+
+  client.print("aa-1;0;1.1;2.2;3.11");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return false;
+    }
+  }
+
+  delay(500);
+
+  client.stop();
+  
+  return true;
 }
 
 void handleRoot() {                          // When URI / is requested, send a web page with a button to toggle the LED
@@ -51,13 +154,33 @@ void handleLogin() {                         // If a POST request is made to URI
     server.send(400, "text/plain", "400: Invalid Request");         // The request is invalid, so send HTTP status 400
     return;
   }
-  if(server.arg("username") == "John Doe" && server.arg("password") == "password123") { // If both the username and the password are correct
-    server.send(200, "text/html", "<h1>Welcome, " + server.arg("username") + "!</h1><p>Login successful</p>");
-  } else {                                                                              // Username and password don't match
-    server.send(401, "text/plain", "401: Unauthorized");
-  }
+//  if(server.arg("username") == "John Doe" && server.arg("password") == "password123") { // If both the username and the password are correct
+//    server.send(200, "text/html", "<h1>Welcome, " + server.arg("username") + "!</h1><p>Login successful</p>");
+//  } else {                                                                              // Username and password don't match
+//    server.send(401, "text/plain", "401: Unauthorized");
+//  }
+
+  ssid = server.arg("username");
+  password = server.arg("password");
+  server.send(200, "text/html", "<h1>connecting to: " + server.arg("username") + " " + server.arg("password"));
 }
 
 void handleNotFound(){
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+  String s = MAIN_page;
+  server.send(200, "text/html", s); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+String readFromSerialIfAvailable() {
+  String responseBuffer = "";
+  
+  if (Serial.available()) {
+    while (Serial.available()) {
+      char charIn = Serial.read();
+      responseBuffer += charIn;
+    }  
+  }
+
+  responseBuffer.trim();
+
+  return responseBuffer;
 }
